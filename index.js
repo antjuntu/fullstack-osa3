4cf7be87011be13879c8b81efeb18499831a0987
antjuntu <antjuntu@gmail.com>
2019-05-10 17:58:13 +0300

    3.15: puhelinluettelo ja tietokanta, step3

--- index.js
+++ index.js
@@ -46,10 +46,13 @@ app.get('/api/persons/:id', (req, res) => {
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = parseInt(req.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  res.status(204).end()
+  Person
+    .findByIdAndRemove(req.params.id)
+    .then(result => {
+      console.log(result)
+      res.status(204).end()
+    })
+    .catch(error => console.log(error))
 })
 
 app.post('/api/persons', (req, res) => {
